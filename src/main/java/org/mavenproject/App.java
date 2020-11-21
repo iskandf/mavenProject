caceb02e8bc2475dc9ddb09f9818727dc46d06c7
fouadyuosef <db@live.de>
2020-11-21 16:52:23 +0100

    initial commit: basic commit

---
+++ src/main/java/org/mavenproject/App.java
@@ -0,0 +1,14 @@
+package     org.mavenproject ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
