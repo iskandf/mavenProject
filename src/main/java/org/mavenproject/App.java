2152bccf297278ba974cca8d62f001cc31ace696
fouadyuosef <db@live.de>
2020-11-28 18:45:51 +0100

    new change

--- src/main/java/org/mavenproject/App.java
+++ src/main/java/org/mavenproject/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "this is the first change" );
+        System.out.println( "this is the second change" );
     }
 }
