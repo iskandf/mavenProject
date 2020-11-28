378b2d7f9866de63cf236fbb9d432748be0c9f7d
Fouad Iskandar <74730076+iskandf@users.noreply.github.com>
2020-11-28 19:04:32 +0100

    Update App.java

--- src/main/java/org/mavenproject/App.java
+++ src/main/java/org/mavenproject/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "this is the second change" );
+        System.out.println( "this is the third change" );
     }
 }
