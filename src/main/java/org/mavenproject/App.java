e898a8eb37fdee771137d9ff597339d137eaccd1
fouadyuosef <db@live.de>
2020-11-28 19:12:23 +0100

    all th changes for new Idea branch

--- src/main/java/org/mavenproject/App.java
+++ src/main/java/org/mavenproject/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "this is the second change" );
+        System.out.println( "changes for new Idea branch" );
     }
 }
